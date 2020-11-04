829b4d5f1a8b554578d6891de67b763018f039b7
MarkNewcomb1 <wlmmn@hotmail.com>
2020-11-04 15:22:53 -0700

    first try

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React, { Component } from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 import TextEditor from './components/TextEditor';
 
-class App extends Component {
-  render() {
+const App = () => {
+
+  const [textArea, setTextArea] = useState([]);
+
+  const addWordToTextArea = (word) => {
+    setTextArea([...textArea, word]);
+  }
+
+  const undoWordFromTextArea = (word) => {
+    const oneRemoved = [...textArea].slice(0, -1);
+    setTextArea(oneRemoved);
+  }
     return (
       <div className="App">
-        <TextEditor />
+        <TextEditor 
+        textArea={textArea}
+        addWordToTextArea={addWordToTextArea}
+        undoWordFromTextArea={undoWordFromTextArea}
+        />
       </div>
     );
-  }
 }
 
 export default App;
--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-class TextEditor extends React.Component {
+const TextEditor = ({textArea, addWordToTextArea, undoWordFromTextArea}) => {
 
-  render() {
+  const [word, setWord] = useState('');
+
+  const handleChange = (e) => {
+    setWord(e.target.value);
+  }
+
+  const handleSubmit = (word) => {
+    addWordToTextArea(word)
+    setWord('');
+  }
     return (
       <React.Fragment>
         <div className="controls">
-          <input className="word-input" type="text" data-testid="word-input" />
-          <button data-testid="append-button">Append</button>
-          <button data-testid="undo-button">Undo</button>
+          <input className="word-input" type="text" data-testid="word-input" onChange={(e)=> handleChange(e)} value={word}/>
+          <button data-testid="append-button" disabled={!word} onClick={(e) => handleSubmit(word)}>Append</button>
+          <button data-testid="undo-button" disabled={!textArea.length} onClick={() => undoWordFromTextArea()}>Undo</button>
         </div>
-        <div className="text-field" data-testid="text-field">REPLACE ME WITH CURRENT TEXT CONTENT</div>
+        <div className="text-field" data-testid="text-field">
+          { textArea.map((t, i) => (
+            <span key={i}>{t}&nbsp;</span>
+          ))}
+        </div>
       </React.Fragment>
     );
-  }
 }
 
 export default TextEditor;
